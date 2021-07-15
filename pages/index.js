692e5e8d394acb28756b9877026e362be36d1575
Daniel Habib <daniel.habib@dataprev.gov.br>
2021-07-15 10:24:56 -0300

    add links para github; add parametro menu mobile

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,73 @@
-import MainGrid from '../src/components/MainGrid'
-import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar(propriedades) {
   console.log(propriedades);
   return (
     <Box>
-      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img
+        src={`https://github.com/${propriedades.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
     </Box>
-  )
+  );
 }
 
 export default function Home() {
-  const usuarioAleatorio = 'danielhvs';
+  const usuarioAleatorio = "danielhvs";
   const pessoasFavoritas = [
-    'juunegreiros',
-    'omariosouto',
-    'peas',
-    'rafaballerini',
-    'marcobrunodev',
-    'felipefialho'
-  ]
+    "katcipis",
+    "mecking",
+    "ebeninca",
+    "claudemirbrum",
+    "apsey",
+    "drrzmr",
+  ];
 
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={"danielhvs"} />
       <MainGrid>
         {/* <Box style="grid-area: profileArea;"> */}
-        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
           <ProfileSidebar githubUser={usuarioAleatorio} />
         </div>
-        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
           <Box>
-            <h1 className="title">
-              Bem vindo(a) 
-            </h1>
+            <h1 className="title">Bem vindo(a), {usuarioAleatorio}</h1>
 
             <OrkutNostalgicIconSet />
           </Box>
         </div>
-        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
-              Pessoas da comunidade ({pessoasFavoritas.length})
+              Pessoas no github ({pessoasFavoritas.length})
             </h2>
 
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
                   <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <a href={`https://github.com/${itemAtual}`} key={itemAtual}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
                     </a>
                   </li>
-                )
+                );
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
-  )
+  );
 }
